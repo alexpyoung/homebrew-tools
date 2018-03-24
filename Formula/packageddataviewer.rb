ef5a95bd3607ec12dcc2fa7edcdc2a486493c402
Travis CI <travis@travis-ci.org>
2018-03-24 18:08:48 +0000

    feat(packageddataviewer): 1.2.0

--- Formula/packageddataviewer.rb
+++ Formula/packageddataviewer.rb
@@ -2,7 +2,7 @@ class Packageddataviewer < Formula
   desc "A CLI for SQLite querying"
   homepage "https://github.com/alexpyoung/PackagedDataViewer"
   url "https://github.com/alexpyoung/PackagedDataViewer/releases/download/1.2.0/PackagedDataViewer-1.2.0.tar.gz"
-  sha256 "f658a73c410615283aa3bb57d69b8302c1e86599f82aa8c35b726210f51d4a48"
+  sha256 "76a95e3a9aaf7d034e6c188c5698ce83e9ef6ae305b8dd9ced5599d04f7f9e3b"
   version "1.2.0"
   bottle :unneeded
   def install
