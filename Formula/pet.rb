dda395301d219bcc1fa27bebcb02a757d8dcc56b
Alex Young <alexyoung1992@gmail.com>
2020-04-26 19:33:11 -0400

    feat(pet): publish v0.3.6

--- Formula/pet.rb
+++ Formula/pet.rb
@@ -1,9 +1,9 @@
 class Pet < Formula
   desc "Unofficial distribution of pet CLI"
   homepage "https://github.com/knqyf263/pet"
-  url "https://github.com/knqyf263/pet/releases/download/v0.3.2/pet_0.3.2_darwin_amd64.tar.gz"
-  sha256 "a15ae670ab17a1a1253da4451fb57c5756874db5f931438f23e95866d8f44fc1"
-  version "0.3.2"
+  url "https://github.com/knqyf263/pet/releases/download/v0.3.6/pet_0.3.6_darwin_amd64.tar.gz"
+  sha256 "10bfe67c3299f51609c7fa8ecf28d22009c29e9a12e6d5541bad297ec83a24e2"
+  version "0.3.6"
   bottle :unneeded
   def install
     bin.install "pet"
